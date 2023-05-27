5138e8fd45850589b6cf14966d48f6610b70540e
JoseAntonioBretonesGarcia <joseantoniobregar@gmail.com>
2023-05-27 02:14:58 +0200

    Section 7 finished.

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,18 +1,60 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
+  public gifList: Gif[] = [];
   private _tagHistory: string[] = [];
+  private apiKey: string = 'VcAe68WofcGXWcOZZrHxtAUoC5Ykqj4J';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+    console.log('Gifs service ready');
+  }
 
   get tagsHistory(): string[] {
     return [...this._tagHistory];
   }
 
   searchTag(tag: string): void {
-    this._tagHistory.unshift(tag);
+    if (tag.length === 0) return;
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('q', tag)
+      .set('limit', 10);
+
+    this.http
+      .get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((res) => {
+        this.gifList = res.data;
+      });
+    this.organizeHistory(tag);
+  }
+
+  private organizeHistory(newTag: string): void {
+    newTag = newTag.toLowerCase();
+
+    if (this._tagHistory.includes(newTag)) {
+      this._tagHistory = this.tagsHistory.filter((tag) => tag !== newTag);
+    }
+    this._tagHistory.unshift(newTag);
+    this._tagHistory = this._tagHistory.splice(0, 10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+    this._tagHistory = JSON.parse(localStorage.getItem('history')!);
+    if (this._tagHistory.length === 0) return;
+    this.searchTag(this.tagsHistory[0]);
   }
 }
