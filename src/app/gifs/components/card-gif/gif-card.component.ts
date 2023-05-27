79c051f39455521ac00504d2d9077d1b98950780
JoseAntonioBretonesGarcia <joseantoniobregar@gmail.com>
2023-05-27 10:10:22 +0200

    Section 8 finished.

---
+++ src/app/gifs/components/card-gif/gif-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interface';
+
+@Component({
+  selector: 'gif-card',
+  templateUrl: 'gif-card.component.html',
+})
+export class GifCardComponent implements OnInit {
+  @Input()
+  public gif!: Gif;
+
+  ngOnInit(): void {
+    if (!this.gif) throw new Error('Gif is required.');
+  }
+}
--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { CardListComponent } from './components/card-list/card-list.component';
+import { GifCardComponent } from './components/card-gif/gif-card.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
-import { CardListComponent } from './components/card-list/card-list.component';
 
 @NgModule({
-  declarations: [HomePageComponent, SearchBoxComponent, CardListComponent],
-  imports: [CommonModule],
+  declarations: [
+    CardListComponent,
+    GifCardComponent,
+    HomePageComponent,
+    SearchBoxComponent,
+  ],
+  imports: [CommonModule, SharedModule],
   exports: [HomePageComponent],
 })
 export class GifsModule {}
